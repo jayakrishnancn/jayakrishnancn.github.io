c4ffe7872572adccd3319bf4d820dba86f2dc3de
jayakrishnancn <jayakrishnancn@gmail.com>
2020-03-17 03:03:42 +0530

    Updates

---
+++ precache-manifest.8886eb37b9379e2e769dc6b49d6cedc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8e74344e14ae493e95457291b01784",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ab59edd2bcbc3c161f28",
+    "url": "/react-portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6b33137aca59c923ee4a",
+    "url": "/react-portfolio/static/css/main.84a23cd1.chunk.css"
+  },
+  {
+    "revision": "ab59edd2bcbc3c161f28",
+    "url": "/react-portfolio/static/js/2.f048743f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-portfolio/static/js/2.f048743f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b33137aca59c923ee4a",
+    "url": "/react-portfolio/static/js/main.7dc412ef.chunk.js"
+  },
+  {
+    "revision": "6c1a97d7eedcd4b749e8",
+    "url": "/react-portfolio/static/js/runtime-main.fcfd7c4a.js"
+  },
+  {
+    "revision": "e0537f826e9c318a43968ae4041a0afc",
+    "url": "/react-portfolio/static/media/profile-pic-compressed.e0537f82.jpg"
+  }
+]);
