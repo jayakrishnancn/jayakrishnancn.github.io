ab190c5ef2eec623a7e799f5e1c04633eb416c4b
jayakrishnancn <jayakrishnancn@gmail.com>
2020-03-17 12:50:19 +0530

    Updates

---
+++ precache-manifest.a28f4028bf11c158b423a7c8e9ce2008.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e7682ce1f562d6a90a7ed484ff9d4f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ab59edd2bcbc3c161f28",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1c1e27604c069a35ffd0",
+    "url": "/portfolio/static/css/main.84a23cd1.chunk.css"
+  },
+  {
+    "revision": "ab59edd2bcbc3c161f28",
+    "url": "/portfolio/static/js/2.f048743f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.f048743f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1e27604c069a35ffd0",
+    "url": "/portfolio/static/js/main.c0ba58d9.chunk.js"
+  },
+  {
+    "revision": "f565fc7b8f77edeea3cf",
+    "url": "/portfolio/static/js/runtime-main.84cdc987.js"
+  },
+  {
+    "revision": "e0537f826e9c318a43968ae4041a0afc",
+    "url": "/portfolio/static/media/profile-pic-compressed.e0537f82.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.8886eb37b9379e2e769dc6b49d6cedc6.js"
+  "/portfolio/precache-manifest.a28f4028bf11c158b423a7c8e9ce2008.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
