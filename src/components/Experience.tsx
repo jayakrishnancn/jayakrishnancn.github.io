46bb8100cde9ae3a52d2a02914ea25075d99a832
jayakrishnan cn <jayakrishnancn@gmail.com>
2021-11-01 22:00:17 +0530

    added exp line on md+

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,7 +5,7 @@ const Experience = () => {
   return (
     <div className="mt-28 max-w-md mx-auto text-gray-500">
       <h1 className="text-center uppercase mb-16 underline">Experience</h1>
-      <div>
+      <div className="exp pb-16">
         <h2 className="mb-2">Envestnet</h2>
         <span className="font-bold text-gray-500">Senior Engineer</span>
         <span className="font-bold text-gray-400 ml-2 pl-2 text-sm border-l-2">
@@ -15,12 +15,9 @@ const Experience = () => {
           Full stack developer for leading Asset Management platform. Experience
           in React, Typescript, Javascript, Java, Spring, Spring boot, AWS.
         </p>
-        <a href="#achievement" className="text-sm float-right">
-          <ArrowSlide>More</ArrowSlide>
-        </a>
       </div>
-      <div>
-        <h2 className="mt-10 mb-2">Tata Consultancy Services</h2>
+      <div className="exp">
+        <h2 className="mt-0 mb-2">Tata Consultancy Services</h2>
         <span className="font-bold text-gray-500">Systems Engineer</span>
         <p className="text-sm my-1 text-justify text-gray-400">
           Worked on modernization of policy Administration system for a leading
@@ -29,9 +26,6 @@ const Experience = () => {
           headquarter), to provide my service.Worked on Performance optimization
           of java based applications.
         </p>
-        <a href="#achievement" className="text-sm float-right">
-          <ArrowSlide>More</ArrowSlide>
-        </a>
       </div>
     </div>
   )
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -74,63 +74,69 @@ const PROJECTS = [
 
 const Projects = () => {
   return (
-    <div
-      id="projects"
-      className="mt-28 mb-10 grid sm:grid-cols-2 md:grid-cols-3 gap-3 text-justify"
-    >
-      {PROJECTS.sort((a, b) => b.year - a.year).map(project => {
-        let { title, year, link, github, content, tags = [] } = project
-        return (
-          <div
-            key={`projects-${title}`}
-            className="flex flex-col mt-3 justify-between group p-5 border text-muted hover:border-red-400 bg-gray-50 rounded-md transform shadow-sm hover:shadow-md transition-all hover:-translate-y-2"
-          >
-            <div className="flex justify-between my-5">
-              <div className="flex items-center">
-                <FaRegFolder size="2em" className="group-hover:text-red-500" />
-                {year && <span className="ml-4">{year}</span>}
+    <>
+      <h1 className="text-center uppercase mt-28 mb-10  underline">Projects</h1>
+      <div
+        id="projects"
+        className="grid sm:grid-cols-2 md:grid-cols-3 gap-3 text-justify"
+      >
+        {PROJECTS.sort((a, b) => b.year - a.year).map(project => {
+          let { title, year, link, github, content, tags = [] } = project
+          return (
+            <div
+              key={`projects-${title}`}
+              className="flex flex-col mt-3 justify-between group p-5 border text-muted hover:border-red-400 bg-gray-50 rounded-md transform shadow-sm hover:shadow-md transition-all hover:-translate-y-2 cursor-default"
+            >
+              <div className="flex justify-between my-5">
+                <div className="flex items-center">
+                  <FaRegFolder
+                    size="2em"
+                    className="group-hover:text-red-500"
+                  />
+                  {year && <span className="ml-4">{year}</span>}
+                </div>
+                <div>
+                  {link && (
+                    <a href={link} target="_blank">
+                      <FaExternalLinkAlt
+                        size="1em"
+                        className="mx-3 hover:text-red-500 text-muted"
+                      />
+                    </a>
+                  )}
+                  {github && (
+                    <a href={github} target="_blank">
+                      <FaGithub
+                        className="mx-2 hover:text-red-500  text-muted"
+                        size="1em"
+                      />
+                    </a>
+                  )}
+                </div>
               </div>
               <div>
-                {link && (
-                  <a href={link} target="_blank">
-                    <FaExternalLinkAlt
-                      size="1em"
-                      className="mx-3 hover:text-red-500 text-muted"
-                    />
-                  </a>
-                )}
-                {github && (
-                  <a href={github} target="_blank">
-                    <FaGithub
-                      className="mx-2 hover:text-red-500  text-muted"
-                      size="1em"
-                    />
-                  </a>
-                )}
+                <h1 className="text-2xl font-bold text-muted group-hover:text-black">
+                  {title}
+                </h1>
+                <p className="felx-1 text-muted group-hover:text-gray-500">
+                  {content}
+                </p>
+              </div>
+              <div className="text-xs flex items-end justify-around flex-wrap">
+                {tags.map((tag, index) => (
+                  <span
+                    key={`tag-${index}`}
+                    className="text-muted group-hover:text-gray-500 inline-block mx-1 leading-6"
+                  >
+                    {tag}
+                  </span>
+                ))}
               </div>
             </div>
-            <div>
-              <h1 className="text-2xl font-bold text-muted group-hover:text-black">
-                {title}
-              </h1>
-              <p className="felx-1 text-muted group-hover:text-gray-500">
-                {content}
-              </p>
-            </div>
-            <div className="text-xs flex items-end justify-around flex-wrap">
-              {tags.map((tag, index) => (
-                <span
-                  key={`tag-${index}`}
-                  className="text-muted group-hover:text-gray-500 inline-block mx-1 leading-6"
-                >
-                  {tag}
-                </span>
-              ))}
-            </div>
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 export default Projects
