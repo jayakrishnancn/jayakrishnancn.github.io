d463e6bb6bad65a25f16d1bc23163228f52e3520
jayakrishnan cn <jayakrishnancn@gmail.com>
2022-05-15 20:19:22 +0530

    tmp

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/seo"
 import { getChaiTime } from "../utils/timeCalc"
 import { GatsbyImage } from "gatsby-plugin-image"
